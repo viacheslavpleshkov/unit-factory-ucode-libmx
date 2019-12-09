ee93cd4ba66756b7b99b180013faa7a4a56d375c
Slava Pleshkov <mail@slavapleshkov.com>
2019-12-09 13:05:27 +0200

    Add files

---
+++ src/mx_isdigit.c
@@ -0,0 +1,9 @@
+#include "libmx.h"
+
+bool mx_isdigit(int c)
+{
+    if (c >= 48 && c <= 57)
+        return true;
+    else    
+        return false;
+}
---
+++ src/mx_pow.c
@@ -0,0 +1,16 @@
+#include "libmx.h"
+
+double mx_pow(double n, unsigned int pow)
+{
+    double result = 1;
+
+    if (pow == 0)
+        return result;
+    else if (pow > 0)
+    {
+        for (unsigned int i = 0; i < pow; i++)
+            result *= n;
+    }
+
+    return result;
+}
